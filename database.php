daaeb405065c8a4347ed623eb174fb9ad5f49bbc
Franciso Pena <project.01@hotmail.com>
2015-06-23 13:28:07 -0400

    Initial commit
    Former-commit-id: 62773d2207b2a8fe6f5f91d3effe6ffdb68948c8
    Former-commit-id: 4b5560d1525d968ee9337dd3737242b11607a112
    Former-commit-id: 052c3993f7ce7e6eb0b942b50d88ee7552832f4f

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'subbedanime';
+$dbpass = '6vvczWBchcXaxVyn';
+$dbname = 'subbedanime';
+
+$conn = mysql_connect ($dbhost, $dbuser, $dbpass) or die ('I cannot connect to the database because: ' . mysql_error());
+mysql_select_db ($dbname);
+?>
---
+++ time.php
@@ -0,0 +1,38 @@
+<?
+// date_default_timezone_set('America/Edmonton');
+function time_elapsed_string($ptime)
+{
+	$time = time();
+	echo 'Now: '.$time;
+	echo '<br/>';	
+	echo 'Time: '.$ptime;
+	echo '<br/>';
+    $etime = $time - $ptime;
+	echo 'Diff: '.$etime;	
+
+    if ($etime < 1)
+    {
+        return '0 seconds';
+    }
+
+    $a = array( 12 * 30 * 24 * 60 * 60  =>  'year',
+                30 * 24 * 60 * 60       =>  'month',
+                24 * 60 * 60            =>  'day',
+                60 * 60                 =>  'hour',
+                60                      =>  'minute',
+                1                       =>  'second'
+                );
+
+    foreach ($a as $secs => $str)
+    {
+        $d = $etime / $secs;
+        if ($d >= 1)
+        {
+            $r = round($d);
+            return $r . ' ' . $str . ($r > 1 ? 's' : '') . ' ago';
+        }
+    }
+}
+echo '<br/>';
+echo time_elapsed_string('1428376419');
+?>
