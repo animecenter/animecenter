7482eb7c19814a12be88d7a806db9ffe587b6cbe
khrizpv <dr_khriz@hotmail.com>
2015-06-24 11:35:40 -0300

    borrar pop ups
    Former-commit-id: c1bdefdce4c96e7bd37259d17274bf8d2140f9e0
    Former-commit-id: e209667fef975d683e1b8bb1d3c5f29a00eb5802

--- footer.php
+++ footer.php
@@ -1,86 +1,71 @@
-<?
-$country = $_SERVER["HTTP_CF_IPCOUNTRY"];
-echo '<meta description="' . $country . '"/>';
-$t1 = array('us', 'ca', 'au', 'gb');
-$c = '';
-if (in_array(strtolower($country), $t1)) {
-    $c = 'T1';
-}
-if ($c === 'T1') { ?>
-    <!-- BEGIN S0008278 POP -->
+    <div id="footer">
+        <div class="menu">
+            <div class="title">AnimeCenter</div>
+            <ul>
+                <?php foreach ($bottom_pages_list as $bottom_page) {
+                    if (isset($bottom_page['p_link']) and $bottom_page['p_link'] != null) {
+                        $link = $bottom_page['p_link'];
+                    } else {
+                        $link = $url . "page.php?page_id=" . $bottom_page['p_id'];
+                    } ?>
+                    <li>
+                        <a href="<?php echo $link; ?>">
+                            <?php echo $bottom_page['p_title'] ?>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+        <!--/menu-->
 
-    <script>
-        var _gunggo = {settings: {siteID: "S0008278", pop: {type: "tab"}}};
-        _gunggo.settings.pop.freqcap = {frequency: 1, duration: 24};
-    </script>
-    <script src="//cdn.directrev.com/js/gp.min.js?s=S0008278"></script>
+        <div class="menu">
+            <div class="title">Popular Anime Series</div>
+            <ul>
+                <?php foreach ($bottom2_pages_list as $bottom_page) {
+                    if (isset($bottom_page['p_link']) and $bottom_page['p_link'] != null) {
+                        $link = $bottom_page['p_link'];
+                    } else {
+                        $link = $url . "page.php?page_id=" . $bottom_page['p_id'];
+                    }
+                    ?>
+                    <li>
+                        <a href="<?php echo $link; ?>">
+                            <?php echo $bottom_page['p_title'] ?>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+        <!--/menu-->
 
-    <!-- END S0008278 POP -->
-<?php } ?>
-
-<div id="footer">
-
-    <div class="menu">
-        <div class="title">AnimeCenter</div>
-        <ul>
-            <?php foreach ($bottom_pages_list as $bottom_page) {
-                if (isset($bottom_page['p_link']) and $bottom_page['p_link'] != null) {
-                    $link = $bottom_page['p_link'];
-                } else {
-                    $link = $url . "page.php?page_id=" . $bottom_page['p_id'];
-                }
-                ?>
-                <li><a href="<?php echo $link; ?>"><?php echo $bottom_page['p_title'] ?></a></li>
-            <?php } ?>
-        </ul>
+        <div class="menu">
+            <div class="title">Affiliates</div>
+            <ul>
+                <?php foreach ($bottom3_pages_list as $bottom_page) {
+                    if (isset($bottom_page['p_link']) and $bottom_page['p_link'] != null) {
+                        $link = $bottom_page['p_link'];
+                    } else {
+                        $link = $url . "page.php?page_id=" . $bottom_page['p_id'];
+                    }
+                    ?>
+                    <li>
+                        <a href="<?php echo $link; ?>">
+                            <?php echo $bottom_page['p_title'] ?>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+        <!--/menu-->
     </div>
-    <!--/menu-->
-
-    <div class="menu">
-        <div class="title">Popular Anime Series</div>
-        <ul>
-            <?php foreach ($bottom2_pages_list as $bottom_page) {
-                if (isset($bottom_page['p_link']) and $bottom_page['p_link'] != null) {
-                    $link = $bottom_page['p_link'];
-                } else {
-                    $link = $url . "page.php?page_id=" . $bottom_page['p_id'];
-                }
-                ?>
-                <li><a href="<?php echo $link; ?>"><?php echo $bottom_page['p_title'] ?></a></li>
-            <?php } ?>
-        </ul>
-    </div>
-    <!--/menu-->
-
-    <div class="menu">
-        <div class="title">Affiliates</div>
-        <ul>
-            <?php foreach ($bottom3_pages_list as $bottom_page) {
-                if (isset($bottom_page['p_link']) and $bottom_page['p_link'] != null) {
-                    $link = $bottom_page['p_link'];
-                } else {
-                    $link = $url . "page.php?page_id=" . $bottom_page['p_id'];
-                }
-                ?>
-                <li><a href="<?php echo $link; ?>"><?php echo $bottom_page['p_title'] ?></a></li>
-            <?php } ?>
-        </ul>
-    </div>
-    <!--/menu-->
-</div><!--/footer-->
-</div><!--wrap-->
+    <!--/footer-->
+</div>
+<!--wrap-->
 
 <?
 $page = $actual_link = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 if ($page != "http://www.animecenter.tv/index.php") { ?>
 <? } ?>
-<script type="text/javascript">
-    $(document).ready(function (e) {
-        $("#ad-close").click(function (e) {
-            $("#popup-ad").remove();
-        });
-    });
-</script>
 
 <script type="text/javascript">
     (function (i, s, o, g, r, a, m) {
@@ -103,4 +88,3 @@ if ($page != "http://www.animecenter.tv/index.php") { ?>
 </html>
 <?php
 require_once("bottom-cache.php");
-?>
