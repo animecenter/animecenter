9f8e0340847fbe6a38d2f69fb899463091264290
khrizpv <dr_khriz@hotmail.com>
2015-10-09 16:11:54 -0300

    remove unnecesary files
    Former-commit-id: f7f056640c66ad30a14b72a7097529c214a926f4
    Former-commit-id: 39056a9a248c6078139b459a675ea10e8cfba28d
    Former-commit-id: 0fcdcb0293d33e5861e8867e614197e176260c70

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -18,7 +18,7 @@
 </script>
 <script src="{{ asset('js/app.js') }}"></script>
 <script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.9.7.js'><\/script>".replace("HOST", location.hostname));
+    document.write('<script async src="http://HOST:3000/browser-sync/browser-sync-client.2.9.7.js"><\/script>'.replace('HOST', location.hostname));
     //]]>
 </script>
 @yield('scripts')
