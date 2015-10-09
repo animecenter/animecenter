ab934d8c9d6ae44ac38b921df851769b89bc10bb
khrizpv <dr_khriz@hotmail.com>
2015-10-09 16:11:54 -0300

    remove unnecesary files
    Former-commit-id: f7f056640c66ad30a14b72a7097529c214a926f4
    Former-commit-id: 39056a9a248c6078139b459a675ea10e8cfba28d

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,17 +1,19 @@
 @include('layouts.index')
 
 @section('content')
-    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-        <div class="title">Sorry, the page you have requested cannot be found.</div>
-        <div class="content">
-            <div>
-                <img src="{{ asset('imgs/404img.jpg') }}">
-                Why? The URL for this Episode/Anime/Page has been changed. Go to our
-                <a href="{{ url('anime-list') }}">Subbed Anime list</a> or
-                <a href="{{ url('anime-list-dubbed') }}">
-                    Dubbed Anime list</a> depending on your preference and look for your anime show new URL or go
-                visit our <a href="{{ url('/') }}">Homepage</a> to find the latest anime episodes.
-            </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <h2 class="page-header">Sorry, the page you have requested cannot be found.</h2>
+        </div>
+        <div class="col-xs-12">
+            <img src="{{ asset('img/404img.jpg') }}">
+            <p>
+                Why? The URL for this Episode/Anime/Page has been changed. You can go to our
+                <a href="{{ url('anime/subbed') }}">Subbed Anime list</a> or
+                <a href="{{ url('anime/dubbed') }}">Dubbed Anime list</a>
+                depending on your preference and look for your anime show new URL or go
+                visit our <a href="{{ url('/') }}">Homepage</a>.
+            </p>
         </div>
     </div>
 @endsection
