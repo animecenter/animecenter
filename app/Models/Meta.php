895eeb437810e8bd7fe0f10ad0e89cd316cd66bb
khrizpv <dr_khriz@hotmail.com>
2015-10-14 19:29:07 -0300

    Anime filters almost finish
    Former-commit-id: 3b488969a7c25bf280565d07f0ed93c22a8eabe0
    Former-commit-id: 91024310a4a701f9f39f63443bad73403ce3afbc

--- app/Models/Meta.php
+++ app/Models/Meta.php
@@ -4,11 +4,62 @@ namespace AC\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * AC\Models\Meta
- *
- */
 class Meta extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'metas';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var string[]
+     */
+    protected $hidden = [''];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var string[]
+     */
+    protected $guarded = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var string[]
+     */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var string[]
+     */
+    protected $casts = [
+        'id'          => 'int',
+        'route'       => 'string',
+        'title'       => 'string',
+        'keywords'    => 'string',
+        'description' => 'string'
+    ];
+
+    /**
+     * The validation rules.
+     *
+     * @var string[]
+     */
+    public $rules = [
+        'id' => 'required|integer|min:1'
+    ];
 }
