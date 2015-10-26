942dbc4de5c2a0f7a5eadd41098bba7cf62704da
khrizpv <dr_khriz@hotmail.com>
2015-10-26 16:21:12 -0300

    Work on statuses and types dashboard area.
    Former-commit-id: e8f2a3cdd1d21d574ef2daf8897eaebc9d1c9749

--- app/Http/Controllers/Dashboard/StatusController.php
+++ app/Http/Controllers/Dashboard/StatusController.php
@@ -2,18 +2,174 @@
 
 namespace AC\Http\Controllers\Dashboard;
 
+use AC\Models\Status;
 use DB;
+use Illuminate\Http\Request;
 
 class StatusController extends DashboardController
 {
+    /**
+     * @var Status
+     */
+    private $status;
+
+    /**
+     * @param Status $status
+     */
+    public function __construct(Status $status)
+    {
+        $this->status = $status;
+    }
+
+    public function index()
+    {
+        return view('dashboard.statuses.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getCreate()
+    {
+        return view('dashboard.statuses.create');
+    }
+
+    /**
+     * Create a new resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postCreate(Request $request)
+    {
+        $status = new $this->status;
+        $status->name = $request['name'];
+        $status->active = $request['active'] === '1' ? 1 : 0;
+        $status->save();
+        $msg = 'Status was created successfully!';
+
+        return redirect()->action('Dashboard\StatusController@index')->with('success', $msg);
+    }
+
+    /**
+     * Show the form for editing a resource.
+     *
+     * @param int $id
+     * @return \Illuminate\View\View
+     */
+    public function getEdit($id = 0)
+    {
+        return view(
+            'dashboard.statuses.edit',
+            ['status' => DB::table('statuses')->where('id', '=', $id)->first()]
+        );
+    }
+
+    /**
+     * Edit a resource.
+     *
+     * @param int $id
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postEdit($id = 0, Request $request)
+    {
+        $status = $this->status->findOrFail($id);
+        $status->name = $request['name'];
+        $status->active = $request['active'] === '1' ? 1 : 0;
+        $status->save();
+        $msg = 'Status was created successfully!';
+
+        return redirect()->action('Dashboard\StatusController@index')->with('success', $msg);
+    }
+
+    /**
+     * Show trash resources.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getTrash()
+    {
+        return view('dashboard.statuses.trash');
+    }
+
+    /**
+     * Trash resource by id.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postTrash($id = 0)
+    {
+        $this->status->findOrFail($id)->delete();
+        $msg = 'Status was trashed successfully!';
+
+        return redirect()->action('Dashboard\StatusController@index')->with('success', $msg);
+    }
+
+    /**
+     * Delete resource by id.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postDelete($id = 0)
+    {
+        $this->status->withTrashed()->findOrFail($id)->forceDelete();
+        $msg = 'Status was deleted successfully!';
+
+        return redirect()->action('Dashboard\StatusController@getTrash')->with('success', $msg);
+    }
+
+    /**
+     * Recover resource from trash by id.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postRecover($id = 0)
+    {
+        $this->status->withTrashed()->findOrFail($id)->restore();
+        $msg = 'Status was recovered successfully!';
+
+        return redirect()->action('Dashboard\StatusController@getTrash')->with('success', $msg);
+    }
+
+    /**
+     * Get resource listing
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getList()
     {
         $url = 'statuses';
-        $list = collect(DB::table('statuses')->get(['id', 'name', 'active']));
+        $list = collect(
+            DB::table('statuses')->where('deleted_at', '=', null)->get(['id', 'name', 'active'])
+        );
         $showColumns = ['name', 'active', 'actions'];
         $searchColumns = ['name', 'active'];
         $orderColumns = ['name', 'active'];
 
         return parent::getDataTableList($url, $list, $showColumns, $searchColumns, $orderColumns);
+    }
+
+    /**
+     * Get trash resource listing
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getListTrash()
+    {
+        $url = 'statuses';
+        $list = collect(
+            DB::table('statuses')->where('deleted_at', '<>', '')->get(['id', 'name', 'active'])
+        );
+        $showColumns = ['name', 'active', 'actions'];
+        $searchColumns = ['name', 'active'];
+        $orderColumns = ['name', 'active'];
+
+        return parent::getDataTableListTrash($url, $list, $showColumns, $searchColumns, $orderColumns);
     }
 }
--- app/Http/Controllers/Dashboard/TypeController.php
+++ app/Http/Controllers/Dashboard/TypeController.php
@@ -2,18 +2,176 @@
 
 namespace AC\Http\Controllers\Dashboard;
 
+use AC\Models\Type;
 use DB;
+use Illuminate\Http\Request;
 
 class TypeController extends DashboardController
 {
+    /**
+     * @var Type
+     */
+    private $type;
+
+    /**
+     * @param Type $type
+     */
+    public function __construct(Type $type)
+    {
+        $this->type = $type;
+    }
+
+    public function index()
+    {
+        return view('dashboard.types.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getCreate()
+    {
+        return view('dashboard.types.create');
+    }
+
+    /**
+     * Create a new resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postCreate(Request $request)
+    {
+        $type = new $this->type;
+        $type->name = $request['name'];
+        $type->model = $request['model'];
+        $type->active = $request['active'] === '1' ? 1 : 0;
+        $type->save();
+        $msg = 'Type was created successfully!';
+
+        return redirect()->action('Dashboard\TypeController@index')->with('success', $msg);
+    }
+
+    /**
+     * Show the form for editing a resource.
+     *
+     * @param int $id
+     * @return \Illuminate\View\View
+     */
+    public function getEdit($id = 0)
+    {
+        return view(
+            'dashboard.types.edit',
+            ['type' => DB::table('types')->where('id', '=', $id)->first()]
+        );
+    }
+
+    /**
+     * Edit a resource.
+     *
+     * @param int $id
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postEdit($id = 0, Request $request)
+    {
+        $type = $this->type->findOrFail($id);
+        $type->name = $request['name'];
+        $type->model = $request['model'];
+        $type->active = $request['active'] === '1' ? 1 : 0;
+        $type->save();
+        $msg = 'Type was created successfully!';
+
+        return redirect()->action('Dashboard\TypeController@index')->with('success', $msg);
+    }
+
+    /**
+     * Show trash resources.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getTrash()
+    {
+        return view('dashboard.types.trash');
+    }
+
+    /**
+     * Trash resource by id.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postTrash($id = 0)
+    {
+        $this->type->findOrFail($id)->delete();
+        $msg = 'Type was trashed successfully!';
+
+        return redirect()->action('Dashboard\TypeController@index')->with('success', $msg);
+    }
+
+    /**
+     * Delete resource by id.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postDelete($id = 0)
+    {
+        $this->type->withTrashed()->findOrFail($id)->forceDelete();
+        $msg = 'Type was deleted successfully!';
+
+        return redirect()->action('Dashboard\TypeController@getTrash')->with('success', $msg);
+    }
+
+    /**
+     * Recover resource from trash by id.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postRecover($id = 0)
+    {
+        $this->type->withTrashed()->findOrFail($id)->restore();
+        $msg = 'Type was recovered successfully!';
+
+        return redirect()->action('Dashboard\TypeController@getTrash')->with('success', $msg);
+    }
+
+    /**
+     * Get resource listing
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getList()
     {
         $url = 'types';
-        $list = collect(DB::table('types')->get(['id', 'name', 'model', 'active']));
+        $list = collect(
+            DB::table('types')->where('deleted_at', '=', null)->get(['id', 'name', 'model', 'active'])
+        );
         $showColumns = ['name', 'model', 'active', 'actions'];
         $searchColumns = ['name', 'model', 'active'];
         $orderColumns = ['name', 'model', 'active'];
 
         return parent::getDataTableList($url, $list, $showColumns, $searchColumns, $orderColumns);
+    }
+
+    /**
+     * Get trash resource listing
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getListTrash()
+    {
+        $url = 'types';
+        $list = collect(
+            DB::table('types')->where('deleted_at', '<>', '')->get(['id', 'name', 'model', 'active'])
+        );
+        $showColumns = ['name', 'model', 'active', 'actions'];
+        $searchColumns = ['name', 'model', 'active'];
+        $orderColumns = ['name', 'model', 'active'];
+
+        return parent::getDataTableListTrash($url, $list, $showColumns, $searchColumns, $orderColumns);
     }
 }
