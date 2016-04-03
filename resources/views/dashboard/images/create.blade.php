fec4a04edf468a3226333682fb5a55118dd49e72
khrizpv <dr_khriz@hotmail.com>
2016-04-03 13:40:01 -0400

    Fix login, and sign-up routes. Added missing meta data, and update import on Authenticate.php
    Former-commit-id: d42196941bfd9d4b3c979f3df6f9de23eda23e0f

---
+++ resources/views/dashboard/images/create.blade.php
@@ -0,0 +1,42 @@
+@extends('dashboard.layouts.main')
+
+@section('content')
+    <div class="bigTitle">Add New - Do NOT include domain in the Link URL</div>
+    <form action="{{ url('admin/images/create') }}" method="post" enctype="multipart/form-data">
+
+        {!! csrf_field() !!}
+
+        <div class="inputNOption">
+            <div class="smallTitle">Big Title:</div>
+            <input name="bigtitle" value="{{ old('bigtitle') }}" type="text" class="textInput"/>
+        </div>
+        <!--/inputNOption-->
+
+        <div class="inputNOption">
+            <div class="smallTitle">Small Title:</div>
+            <input name="smalltitle" value="{{ old('smalltitle') }}" type="text" class="textInput"/>
+        </div>
+        <!--/inputNOption-->
+
+        <div class="inputNOption">
+            <div class="smallTitle">Link:</div>
+            <input name="link" value="{{ old('link') }}" type="text" class="textInput"/>
+        </div>
+        <!--/inputNOption-->
+
+        <div class="inputUpload">
+            <div class="smallTitle">Image File (640 * 360):</div>
+            <input name="file" value="{{ old('file') }}" type="file" class="file"/>
+        </div>
+        <!--/inputUpload-->
+
+        <div class="inputTextarea">
+            <div class="smallTitle">Description:</div>
+            <textarea class="textarea" name="desc" rows="30">{{ old('desc') }}</textarea>
+        </div>
+        <!--/inputTextarea-->
+
+        <input type="submit" id="submit" value="Add"/>
+
+    </form>
+@endsection
---
+++ resources/views/dashboard/images/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('dashboard.layouts.main')
+
+@section('content')
+    <div class="bigTitle">Add New - Do NOT include domain in the Link URL</div>
+    <form action="{{ url('admin/images/edit/' . $image['id']) }}" method="post" enctype="multipart/form-data">
+
+        {!! csrf_field() !!}
+
+        <div class="inputNOption">
+            <div class="smallTitle">Big Title:</div>
+            <input name="bigtitle" value="{{ old('bigtitle') ? old('bigtitle') : $image['bigtitle'] }}"
+                   type="text" class="textInput"/>
+        </div>
+        <!--/inputNOption-->
+
+        <div class="inputNOption">
+            <div class="smallTitle">Small Title:</div>
+            <input name="smalltitle" value="{{ old('smalltitle') ? old('smalltitle') : $image['smalltitle'] }}"
+                   type="text" class="textInput"/>
+        </div>
+        <!--/inputNOption-->
+
+        <div class="inputNOption">
+            <div class="smallTitle">Link: <br/></div>
+            <input name="link" value="{{ old('link') ? old('link') : $image['link'] }}" type="text" class="textInput"/>
+        </div>
+        <!--/inputNOption-->
+
+        <div class="inputUpload">
+            <div class="smallTitle">Image File (640 * 360):</div>
+            <img src="{{ asset("images/" . $image['file']) }}" width="80" height="80"/>
+            <input name="new_file" value="{{ old('new_file') }}" type="file" class="file"/>
+            <input name="file" value="{{ $image['file'] }}" type="hidden" class="file"/>
+        </div>
+        <!--/inputUpload-->
+
+        <div class="inputTextarea">
+            <div class="smallTitle">Description:</div>
+            <textarea class="textarea" name="desc" rows="30">{{ old('desc') ? old('desc') : $image['desc'] }}</textarea>
+        </div>
+        <!--/inputTextarea-->
+
+        <input type="submit" id="submit" value="Update"/>
+
+    </form>
+@endsection
