5e0e56cbd4c752ca0a01135b736d1b3daeaef65a
khrizpv <dr_khriz@hotmail.com>
2016-04-10 09:56:31 -0400

    WIP V1 - typehint object parameters
    Former-commit-id: 3b9597497af5ddbbc4011512d709a1193056bab1

--- app/Repositories/EloquentAnimeRepository.php
+++ app/Repositories/EloquentAnimeRepository.php
@@ -6,6 +6,11 @@ use AC\Models\Anime;
 use AC\Repositories\EloquentUserRepository as User;
 use Cache;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Http\Request;
 
 class EloquentAnimeRepository
@@ -59,17 +64,17 @@ class EloquentAnimeRepository
         $this->updateViews($slug);
 
         return $this->anime->with([
-            'classification' => function ($query) {
+            'classification' => function (BelongsTo $query) {
                 $query->select(['id', 'name']);
-            }, 'episodes' => function ($query) {
+            }, 'episodes' => function (HasMany $query) {
                 $query->orderBy('number', 'asc');
-            }, 'genres' => function ($query) {
+            }, 'genres' => function (BelongsToMany $query) {
                 $query->select(['id', 'name']);
-            }, 'producers' => function ($query) {
+            }, 'producers' => function (BelongsToMany $query) {
                 $query->select(['id', 'name']);
-            }, 'calendarSeason' => function ($query) {
+            }, 'calendarSeason' => function (BelongsTo $query) {
                 $query->select(['id', 'name']);
-            }, 'type' => function ($query) {
+            }, 'type' => function (BelongsTo $query) {
                 $query->select(['id', 'name']);
             },
         ])->where('slug', '=', $slug)->firstOrFail();
@@ -108,7 +113,7 @@ class EloquentAnimeRepository
             $anime = $this->getByGenres($anime, explode(',', $parameters['genres']));
         }
         if ($request->has('producer')) {
-            $anime = $this->getByProducer($anime, $parameters['producer']);
+            $anime = $this->getByProducers($anime, $parameters['producer']);
         }
         if ($request->has('classification')) {
             $anime = $this->getByClassification($anime, $parameters['classification']);
@@ -138,33 +143,31 @@ class EloquentAnimeRepository
         if (preg_match('/^([a-z])$/', $letter) === 1) {
             return $anime->where('title', 'like', $letter.'%');
         }
-        if ($letter === 'all') {
-            return $anime;
+        if ($letter !== 'all') {
+            abort(404, $letter.' was not found');
         }
-        abort(404, $letter.' was not found');
+        return $anime;
     }
 
     /**
      * Get anime by language.
      *
      * @param $anime
-     * @param string $language
+     * @param string $translation
      *
      * @return mixed
      */
-    public function getByLanguage($anime, $language = '')
+    public function getByLanguage($anime, $translation = '')
     {
-        if ($language === 'subbed') {
-            return $anime->whereHas('episodes.mirrors', function ($query) {
-                $query->where('mirrors.translation', '=', 'subbed');
-            });
+        $translations = ['subbed', 'dubbed'];
+        if (!in_array($translation, $translations)) {
+            // TODO: log current user trying to access unavailable translation...
+            abort(404, 'Unavailable translation');
         }
-        if ($language === 'dubbed') {
-            return $anime->whereHas('episodes.mirrors', function ($query) {
-                $query->where('mirrors.translation', '=', 'dubbed');
-            });
-        }
-        abort(404, $language.' was not found');
+
+        return $anime->whereHas('episodes.mirrors', function (Builder $query) use ($translation) {
+            $query->where('mirrors.translation', '=', $translation);
+        });
     }
 
     /**
@@ -177,7 +180,7 @@ class EloquentAnimeRepository
      */
     public function getByType($anime, $id = 0)
     {
-        return $anime->whereHas('type', function ($query) use ($id) {
+        return $anime->whereHas('type', function (Builder $query) use ($id) {
             $query->where('types.id', '=', $id);
         });
     }
@@ -192,7 +195,7 @@ class EloquentAnimeRepository
      */
     public function getByCalendarSeason($anime, $id = 0)
     {
-        return $anime->whereHas('calendarSeason', function ($query) use ($id) {
+        return $anime->whereHas('calendarSeason', function (Builder $query) use ($id) {
             $query->where('calendar_seasons.id', '=', $id);
         });
     }
@@ -201,15 +204,13 @@ class EloquentAnimeRepository
      * Get anime by Year.
      *
      * @param $anime
-     * @param int $id
+     * @param int $year
      *
      * @return mixed
      */
-    public function getByYear($anime, $id = 0)
+    public function getByYear($anime, $year = 0)
     {
-        return $anime->whereHas('year', function ($query) use ($id) {
-            $query->where('years.id', '=', $id);
-        });
+        return $anime->where('animes.year', '=', $year);
     }
 
     /**
@@ -222,24 +223,24 @@ class EloquentAnimeRepository
      */
     public function getByGenres($anime, $genres = [])
     {
-        return $anime->whereHas('genres', function ($query) use ($genres) {
+        return $anime->whereHas('genres', function (Builder $query) use ($genres) {
             foreach ($genres as $genre) {
                 $query->where('genres.id', $genre);
             }
-        })->get();
+        });
     }
 
     /**
-     * Get anime by producer.
+     * Get anime by producers.
      *
      * @param $anime
      * @param int $id
      *
      * @return mixed
      */
-    public function getByProducer($anime, $id = 0)
+    public function getByProducers($anime, $id = 0)
     {
-        return $anime->whereHas('producers', function ($query) use ($id) {
+        return $anime->whereHas('producers', function (Builder $query) use ($id) {
             $query->where('producers.id', '=', $id);
         });
     }
@@ -254,7 +255,7 @@ class EloquentAnimeRepository
      */
     public function getByClassification($anime, $id = 0)
     {
-        return $anime->whereHas('classification', function ($query) use ($id) {
+        return $anime->whereHas('classification', function (Builder $query) use ($id) {
             $query->where('classifications.id', '=', $id);
         });
     }
@@ -294,33 +295,20 @@ class EloquentAnimeRepository
         }
         if ($translation === 'all') {
             return $this->anime->with([
-                'episode' => function ($query) use ($episodeNumber) {
+                'episode' => function (HasOne $query) use ($episodeNumber) {
                     $query->where('number', '=', $episodeNumber)->firstOrFail();
                 },
                 'episode.mirrors.mirrorSource',
             ])->where('slug', '=', $animeSlug)->firstOrFail();
         }
-        if ($translation === 'subbed') {
-            return $this->anime->has('episode.mirrors.mirrorSource')->with([
-                'episode' => function ($query) use ($episodeNumber) {
-                    $query->where('episodes.number', '=', $episodeNumber)->firstOrFail();
-                },
-                'episode.mirrors' => function ($query) {
-                    $query->with(['mirrorSource' => function ($query) {
-                        $query->orderBy('mirror_sources.id', 'ASC');
-                    }])->where('mirrors.translation', '=', 'subbed');
-                },
-            ])->where('slug', '=', $animeSlug)->firstOrFail();
-        }
-
-        return $this->anime->with([
-            'episode' => function ($query) use ($episodeNumber) {
-                $query->where('number', '=', $episodeNumber)->firstOrFail();
+        return $this->anime->has('episode.mirrors.mirrorSource')->with([
+            'episode' => function (HasOne $query) use ($episodeNumber) {
+                $query->where('episodes.number', '=', $episodeNumber)->firstOrFail();
             },
-            'episode.mirrors' => function ($query) {
-                $query->with(['mirrorSource' => function ($query) {
+            'episode.mirrors' => function (HasMany $query) use ($translation) {
+                $query->with(['mirrorSource' => function (BelongsTo $query) use ($translation) {
                     $query->orderBy('mirror_sources.id', 'ASC');
-                }])->where('mirrors.translation', '=', 'dubbed');
+                }])->where('mirrors.translation', '=', $translation);
             },
         ])->where('slug', '=', $animeSlug)->firstOrFail();
     }
@@ -337,7 +325,7 @@ class EloquentAnimeRepository
      */
     public function getMirror($animeSlug = '', $episodeNumber = 0, $translation = '', $mirrorID = '')
     {
-        return $this->anime->with(['episode.mirror'])->whereHas('episode.mirror', function ($query) use ($episodeNumber, $translation, $mirrorID) {
+        return $this->anime->with(['episode.mirror'])->whereHas('episode.mirror', function (Builder $query) use ($episodeNumber, $translation, $mirrorID) {
             $query->where('mirrors.id', '=', $mirrorID)
                 ->where('mirrors.translation', '=', $translation)->where('episodes.number', '=', $episodeNumber);
         })->where('slug', '=', $animeSlug)->firstOrFail();
