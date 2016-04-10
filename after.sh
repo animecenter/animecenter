efebacc063510b4eacddec5a9935ffa009e5d168
Christian Pena <dr_khriz@hotmail.com>
2016-04-10 00:00:13 -0400

    Move some commands from composer.json to after.sh.

--- after.sh
+++ after.sh
@@ -5,11 +5,10 @@
 # be run after the Homestead machine is provisioned.
 
 cd /home/vagrant/animecenter
-mysql -u homestead -psecret homestead < database/ac.sql
-mysql -u homestead -psecret homestead1 < database/ac1.sql
-mysql -u homestead -psecret homesteadold < database/animecenter.sql
 composer install --no-ansi --no-interaction --no-progress --optimize-autoloader
-# php artisan migrate --seed --env=local
+cp .env.example .env
+php artisan key:generate
+php artisan migrate --seed --env=local
 sudo npm install -g npm gulp bower --no-color --progress=false
 npm install --no-bin-links --no-color --progress=false --silent
 npm rebuild --no-bin-links --no-color --progress=false
